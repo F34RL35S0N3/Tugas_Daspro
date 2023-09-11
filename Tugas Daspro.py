4ddf15752913c6e92288b7181035ad98027a8b32
F34RL35S0N3 <124524511+F34RL35S0N3@users.noreply.github.com>
2023-09-11 08:59:40 +0700

    Add files via upload

---
+++ Tugas Daspro.py
@@ -0,0 +1,425 @@
+
+ # 1.NIM MAHASISWA MATEMATIKA
+
+nim1 =	123160001
+nim2 =	123160002
+nim3 =	123160003
+nim4 =	123160004
+nim5 =	123160005
+nim6 =	123160006
+nim7 =	123160007
+nim8 =	123160008
+nim9 =	123160009
+nim10 =	123160010
+nim11 =	123160011
+nim12 =	123160012
+nim13 =	123160013
+nim14 =	123160014
+nim15 =	123160015
+nim16 =	123160016
+nim17 =	123160017
+nim18 = 123160018
+nim19 =	123160019
+nim20 =	123160020
+nim21 =	123160021
+nim22 =	123160022
+nim23 =	123160023
+nim24 =	123160024
+nim25 =	123160025
+nim26 =	123160026
+nim27 =	123160027
+nim28 =	123160028
+nim29 =	123160029
+nim30 =	123160030
+nim31 =	123160031
+nim32 =	123160032
+nim33 =	123160033
+nim34 =	123160034
+nim35 =	123160035
+nim36 =	123160036
+nim37 =	123160037
+nim38 =	123160038
+nim39 =	123160039
+nim40 =	123160040
+nim41 =	123160041
+nim42 =	123160042
+nim43 =	123160043
+nim44 =	123160044
+nim45 =	123160045
+nim46 =	123160046
+nim47 =	123160047
+nim48 =	123160048
+nim49 =	123160049
+nim50 =	123160050
+nim51 =	123160051
+nim52 =	123160052
+nim53 =	123160053
+nim54 =	123160054
+nim55 =	123160055
+nim56 =	123160056
+nim57 =	123160057
+nim58 =	123160058
+nim59 =	123160059
+nim60 =	123160060
+nim61 =	123160061
+nim62 =	123160062
+nim63 =	123160063
+nim64 =	123160064
+nim65 =	123160065
+nim66 =	123160066
+nim67 =	123160067
+nim68 =	123160068
+nim69 =	123160069
+nim70 =	123160070
+nim71 =	123160071
+nim72 =	123160072
+nim73 =	123160073
+nim74 =	123160074
+nim75 =	123160075
+nim76 =	123160076
+nim77 = 123160077
+nim78 =	123160078
+nim79 =	123160079
+nim80 =	123160080
+
+
+# 2.NAMA MAHASISWA
+nama1 =	"LENY PUSPITA NINGRUM"
+nama2 =	"Safitri Dwi Utari"
+nama3 =	"Rolasni Rohaida Br Sidabutar"
+nama4 =	"Anisha Rahmadani Suwandi"
+nama5 =	"RANI TIARA DEWI"
+nama6 =	"SAWAL RAMADIAN"
+nama7 =	"DYAH KUSUMA WARDANI"
+nama8 =	"Silvi Agustiani"
+nama9 =	"ANANDA HIDAYAH PURNAMA"
+nama10 = "ELSA AMALIA"
+nama11 = "JOVITA RIDIANA PUTRI"
+nama12 = "Khansa Hana Nadhifah"
+nama13 = "DIAZTI ANY DWI HERLAMBANG"
+nama14 = "PUTRI NABILAH PAHROZI"
+nama15 = "WITA RESKINA BR. TAMPUBOLON"
+nama16 = "SABELA BUDI UTAMI"
+nama17 = "William Yuen"
+nama18 = "FITRIANA SEMBIRING"
+nama19 = "SISKA VALENTINA"
+nama20 = "Amilia Fatimah Nuraini"
+nama21 = "FEBRI YURITA SARI"
+nama22 = "ARTIVA NOVA"
+nama23 = "Rani Syifa Rahimarahman"
+nama24 = "DESTI PUJI ASTUTI"
+nama25 = "QONITA DWI AULIA"
+nama26 = "Sherly Meilia Riani"
+nama27 = "ISMA HIMATUL AFIFAH"
+nama28 = "Oktavia Nurvita Darmayanti"
+nama29 = "IMA PARRONA SIMANJUNTAK"
+nama30 = "Laura Siska Amanda"
+nama31 = "EMILIZA OKTAVIA RAMADANI"
+nama32 = "AHMAD AFIF FARDANI"
+nama33 = "Fitria Dwi Anggraini"
+nama34 = "LARAS AYU PRIYADI"
+nama35 = "PUTRI RATNA SARI"
+nama36 = "Regita Nur Alifia"
+nama37 = "Axel Hutagalung"
+nama38 = "TIANESA MARCHELLINE PAKPAHAN"
+nama39 = "JONATHAN DWI PUTRA WINARTO"
+nama40 = "MUHAMMAD FIZAI ZAKARIA SAPUTRA"
+nama41 = "FEBRIANTI AMELIA PUTRI"
+nama42 = "JONATHAN KEVIN BINSAR PANGARIBUAN"
+nama43 = "RACHMAT DWIKY DARMAWAN"
+nama44 = "Dian Monalisa Nainggolan"
+nama45 = "Ningrum Nurhana"
+nama46 = "Windi Anggriani"
+nama47 = "GLORIA MISCHELIN ROTHUA SEPTIANI"
+nama48 = "EUGENIA BARUS"
+nama49 = "DAVINA SALSABILLA"
+nama50 = "NOVITA SILALAHI"
+nama51 = "JIHAN FADHIA PUTRI HAMIDI"
+nama52 = "GIZKA FADHILA HUSNI"
+nama53 = "Aisah Mulya Ningrum"
+nama54 = "JOY PATIARMA SITUMORANG"
+nama55 = "MUHAMMAD RIFKY RAMADHAN SIREGAR"
+nama56 = "Ester Aulia Sidabutar"
+nama57 = "Shefira Leli Hasanah"
+nama58 = "NAYLA NURUL AISYAH"
+nama59 = "ARISTA DESTIANA"
+nama60 = "ANTASIA RAHMAWATI"
+nama61 = "AMELIA OKTAVIANI"
+nama62 = "NURMA WIDIA SAPUTRI"
+nama63 = "CRISTIANI ANJELINA PURBA"
+nama64 = "AHMAD JOAN AL GHOZY"
+nama65 = "Citra Purba"
+nama66 = "Mukhlishah Fithaah"
+nama67 = "JELITA ANASTASHA SIDABUTAR"
+nama68 = "ABDUL ROZAK"
+nama69 = "THALIA SASI KIRANA DEWI"
+nama70 = "ANGELINE CHRISTY ARUAN"
+nama71 = "SASI KIRANA ZAHRANI"
+nama72 = "RIDHO ELYEZER SIHOMBING"
+nama73 = "REVA LENA"
+nama74 = "Desmarini"
+nama75 = "WINDA SUHARTINI AZAHRA"
+nama76 = "Eriza Virginia Sihite"
+nama77 = "ARIEL SITUMORANG"
+nama78 = "RAIHAN ED BAGASKORO"
+nama79 = "Anggi Renata Br. Barus"
+nama80 = "Nanda Baipri Damanik"
+
+
+
+
+# 3.EMAIL MAHASISWA
+email1 =	"leny.123160001@student.itera.ac.id"
+email2 =	"safitri.123160002@student.itera.ac.id"
+email3 =	"rolasni.123160003@student.itera.ac.id"
+email4 =	"anisha.123160004@student.itera.ac.id"
+email5 =	"rani.123160005@student.itera.ac.id"
+email6 =	"sawal.123160006@student.itera.ac.id"
+email7 =	"dyah.123160007@student.itera.ac.id"
+email8 =	"silvi.123160008@student.itera.ac.id"
+email9 =	"ananda.123160009@student.itera.ac.id"
+email10 =	"elsa.123160010@student.itera.ac.id"
+email11	=   "jovita.123160011@student.itera.ac.id"
+email12 =	"khansa.123160012@student.itera.ac.id"
+email13 =	"diazti.123160013@student.itera.ac.id"
+email14 =	"putri.123160014@student.itera.ac.id"
+email15 =	"wita.123160015@student.itera.ac.id"
+email16 =	"sabela.123160016@student.itera.ac.id"
+email17 =	"wiliam.123160017@student.itera.ac.id"
+email18 =	"fitriana.123160018@student.itera.ac.id"
+email19 =	"siska.123160019@student.itera.ac.id"
+email20 =	"amilia.123160020@student.itera.ac.id"
+email21 =	"febri.123160021@student.itera.ac.id"
+email22 =	"artiva.123160022@student.itera.ac.id"
+email23 =	"rani.123160023@student.itera.ac.id"
+email24 =	"desti.123160024@student.itera.ac.id"
+email25 =	"qonita.123160025@student.itera.ac.id"
+email26 =	"sherly.123160026@student.itera.ac.id"
+email27 =	"isma.123160027@student.itera.ac.id"
+email28 =	"oktavia.123160028@student.itera.ac.id"
+email29 =	"ima.123160027@student.itera.ac.id"
+email30 =	"laura.123160030@student.itera.ac.id"
+email31 =	"emiliza.123160031@student.itera.ac.id"
+email32 =	"ahmad.123160032@student.itera.ac.id"
+email33 =	"fitria.123160033@student.itera.ac.id"
+email34 =	"laras.123160034@student.itera.ac.id"
+email35 =	"putri.123160014@student.itera.ac.id"
+email36 =	"regita.123160036@student.itera.ac.id"
+email37 =	"axel.123160037@student.itera.ac.id"
+email38 =	"tianesa.123160038@student.itera.ac.id"
+email39 =	"jonathan.123160039@student.itera.ac.id"
+email40 =	"muhammad.123160040@student.itera.ac.id"
+email41 =	"febrianti.123160021@student.itera.ac.id"
+email42 =	"jonathan.123160042@student.itera.ac.id"
+email43 =	"rachmat.123160043@student.itera.ac.id"
+email44 =	"dian.123160044@student.itera.ac.id"
+email45 =	"ningrum.123160045@student.itera.ac.id"
+email46 =	"windi.123160046@student.itera.ac.id"
+email47 =	"gloria.123160047@student.itera.ac.id"
+email48 =	"eugenia.123160048@student.itera.ac.id"
+email49 =	"davina.123160049@student.itera.ac.id"
+email50 =	"novita.123160050@student.itera.ac.id"
+email51 =	"jihan.123160051@student.itera.ac.id"
+email52 =	"gizka.123160052@student.itera.ac.id"
+email53 =	"aisah.123160053@student.itera.ac.id"
+email54 =	"joy.123160054@student.itera.ac.id"
+email55 =	"muhammad.123160055@student.itera.ac.id"
+email56 =	"ester.123160056@student.itera.ac.id"
+email57 =	"shefira.123160057@student.itera.ac.id"
+email58 =	"nayla.123160058@student.itera.ac.id"
+email59 =	"arista.123160059@student.itera.ac.id"
+email60 =	"antasia.123160060@student.itera.ac.id"
+email61 =	"amelia.123160061@student.itera.ac.id"
+email62 =	"nurma.123160062@student.itera.ac.id"
+email63 =	"cristiani.123160063@student.itera.ac.id"
+email64 =	"ahmad.123160032@student.itera.ac.id"
+email65 =	"citra.123160065@student.itera.ac.id"
+email66 =	"mukhlishah.123160066@student.itera.ac.id"
+email67 =	"jelita.123160067@student.itera.ac.id"
+email68 =	"abdul.123160068@student.itera.ac.id"
+email69 =	"thalia.123160069@student.itera.ac.id"
+email70 =	"angeline123160070@student.itera.ac.id"
+email71 =	"sasi.123160071@student.itera.ac.id"
+email72 =	"ridho.123160072@student.itera.ac.id"
+email73 =	"reva.123160073@student.itera.ac.id"
+email74 =	"desmarini.123160074@student.itera.ac.id"
+email75 =	"winda.123160075@student.itera.ac.id"
+email76 =	"eriza.123160076@student.itera.ac.id"
+email77 =	"ariel.123160077@student.itera.ac.id"
+email78 =	"raihan.123160078@student.itera.ac.id"
+email79 =	"anggi.123160079@student.itera.ac.id"
+email80 =	"nanda.123160080@student.itera.ac.id"
+
+    
+
+# 4.INPUT MAHASISWA
+nama = input("Namanya Siapa?")
+email = input("Masukan email anda?")
+
+if email == email1:
+    print("Hallo",nama1,"NIM kamu adalah",nim1)
+elif email == email2:
+    print("Hallo",nama2,"NIM kamu adalah",nim2)
+elif email == email3:
+    print("Hallo",nama3,"NIM kamu adalah",nim3)
+elif email == email4:
+    print("Hallo",nama4,"NIM kamu adalah",nim4)
+elif email == email5:
+    print("Hallo",nama5,"NIM kamu adalah",nim5)
+elif email == email6:
+    print("Hallo",nama6,"NIM kamu adalah",nim6)
+elif email == email7:
+    print("Hallo",nama7,"NIM kamu adalah",nim7)
+elif email == email8:
+    print("Hallo",nama8,"NIM kamu adalah",nim8)
+elif email == email9:
+    print("Hallo",nama9,"NIM kamu adalah",nim9)
+elif email == email10:
+    print("Hallo",nama10,"NIM kamu adalah",nim10)
+elif email == email11:
+    print("Hallo",nama11,"NIM kamu adalah",nim11)
+elif email == email12:
+    print("Hallo",nama12,"NIM kamu adalah",nim12)
+elif email == email13:
+    print("Hallo",nama13,"NIM kamu adalah",nim13)
+elif email == email14:
+    print("Hallo",nama14,"NIM kamu adalah",nim14)
+elif email == email15:
+    print("Hallo",nama15,"NIM kamu adalah",nim15)
+elif email == email16:
+    print("Hallo",nama16,"NIM kamu adalah",nim16)
+elif email == email17:
+    print("Hallo",nama17,"NIM kamu adalah",nim17)
+elif email == email18:
+    print("Hallo",nama18,"NIM kamu adalah",nim18)
+elif email == email19:
+    print("Hallo",nama19,"NIM kamu adalah",nim19)
+elif email == email20:
+    print("Hallo",nama20,"NIM kamu adalah",nim20)
+elif email == email21:
+    print("Hallo",nama21,"NIM kamu adalah",nim21)
+elif email == email22:
+    print("Hallo",nama22,"NIM kamu adalah",nim22)
+elif email == email23:
+    print("Hallo",nama23,"NIM kamu adalah",nim23)
+elif email == email24:
+    print("Hallo",nama24,"NIM kamu adalah",nim24)
+elif email == email25:
+    print("Hallo",nama25,"NIM kamu adalah",nim25)
+elif email == email26:
+    print("Hallo",nama26,"NIM kamu adalah",nim26)
+elif email == email27:
+    print("Hallo",nama27,"NIM kamu adalah",nim27)
+elif email == email28:
+    print("Hallo",nama28,"NIM kamu adalah",nim28)
+elif email == email29:
+    print("Hallo",nama29,"NIM kamu adalah",nim29)
+elif email == email30:
+    print("Hallo",nama30,"NIM kamu adalah",nim30)
+elif email == email31:
+    print("Hallo",nama31,"NIM kamu adalah",nim31)
+elif email == email32:
+    print("Hallo",nama32,"NIM kamu adalah",nim32)
+elif email == email33:
+    print("Hallo",nama33,"NIM kamu adalah",nim33)
+elif email == email34:
+    print("Hallo",nama34,"NIM kamu adalah",nim34)
+elif email == email35:
+    print("Hallo",nama35,"NIM kamu adalah",nim35)
+elif email == email36:
+    print("Hallo",nama36,"NIM kamu adalah",nim36)
+elif email == email37:
+    print("Hallo",nama37,"NIM kamu adalah",nim37)
+elif email == email38:
+    print("Hallo",nama38,"NIM kamu adalah",nim38)
+elif email == email39:
+    print("Hallo",nama39,"NIM kamu adalah",nim39)
+elif email == email40:
+    print("Hallo",nama40,"NIM kamu adalah",nim40)
+elif email == email41:
+    print("Hallo",nama41,"NIM kamu adalah",nim41)
+elif email == email42:
+    print("Hallo",nama42,"NIM kamu adalah",nim42)
+elif email == email43:
+    print("Hallo",nama43,"NIM kamu adalah",nim43)
+elif email == email44:
+    print("Hallo",nama44,"NIM kamu adalah",nim44)
+elif email == email45:
+    print("Hallo",nama45,"NIM kamu adalah",nim45)
+elif email == email46:
+    print("Hallo",nama46,"NIM kamu adalah",nim46)
+elif email == email47:
+    print("Hallo",nama47,"NIM kamu adalah",nim47)
+elif email == email48:
+    print("Hallo",nama48,"NIM kamu adalah",nim48)
+elif email == email49:
+    print("Hallo",nama49,"NIM kamu adalah",nim49)
+elif email == email50:
+    print("Hallo",nama50,"NIM kamu adalah",nim50)
+elif email == email51:
+    print("Hallo",nama51,"NIM kamu adalah",nim51)
+elif email == email52:
+    print("Hallo",nama52,"NIM kamu adalah",nim52)
+elif email == email53:
+    print("Hallo",nama53,"NIM kamu adalah",nim53)
+elif email == email54:
+    print("Hallo",nama54,"NIM kamu adalah",nim54)
+elif email == email55:
+    print("Hallo",nama55,"NIM kamu adalah",nim55)
+elif email == email56:
+    print("Hallo",nama56,"NIM kamu adalah",nim56)
+elif email == email57:
+    print("Hallo",nama57,"NIM kamu adalah",nim57)
+elif email == email58:
+    print("Hallo",nama58,"NIM kamu adalah",nim58)
+elif email == email59:
+    print("Hallo",nama59,"NIM kamu adalah",nim59)
+elif email == email60:
+    print("Hallo",nama60,"NIM kamu adalah",nim60)
+elif email == email61:
+    print("Hallo",nama61,"NIM kamu adalah",nim61)
+elif email == email62:
+    print("Hallo",nama62,"NIM kamu adalah",nim62)
+elif email == email63:
+    print("Hallo",nama63,"NIM kamu adalah",nim63)
+elif email == email64:
+    print("Hallo",nama64,"NIM kamu adalah",nim64)
+elif email == email65:
+    print("Hallo",nama65,"NIM kamu adalah",nim65)
+elif email == email66:
+    print("Hallo",nama66,"NIM kamu adalah",nim66)
+elif email == email67:
+    print("Hallo",nama67,"NIM kamu adalah",nim67)
+elif email == email68:
+    print("Hallo",nama68,"NIM kamu adalah",nim68)
+elif email == email69:
+    print("Hallo",nama69,"NIM kamu adalah",nim69)
+elif email == email70:
+    print("Hallo",nama70,"NIM kamu adalah",nim70)
+elif email == email71:
+    print("Hallo",nama71,"NIM kamu adalah",nim71)
+elif email == email72:
+    print("Hallo",nama72,"NIM kamu adalah",nim72)
+elif email == email73:
+    print("Hallo",nama73,"NIM kamu adalah",nim73)
+elif email == email74:
+    print("Hallo",nama74,"NIM kamu adalah",nim74)
+elif email == email75:
+    print("Hallo",nama75,"NIM kamu adalah",nim75)
+elif email == email76:
+    print("Hallo",nama76,"NIM kamu adalah",nim76)
+elif email == email77:
+    print("Hallo",nama77,"NIM kamu adalah",nim77)
+elif email == email78:
+    print("Hallo",nama78,"NIM kamu adalah",nim78)
+elif email == email79:
+    print("Hallo",nama79,"NIM kamu adalah",nim79)
+elif email == email80:
+    print("Hallo",nama80,",NIM kamu adalah",nim80)
+
+# 5.jika bukan anak matematika
+else:
+    print("Anda bukanlah anak Matematika angkatan 2023!!!")
+
+
+print("ini adalah akhir dari program")
